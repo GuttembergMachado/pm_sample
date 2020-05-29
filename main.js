26ebdf971caa3634efe03e59af8685a03319612b
Guttemberg Meirelles Machado <guttemberg@gmail.com>
2020-05-29 16:55:54 -0300

    update

--- main.js
+++ main.js
@@ -10,10 +10,10 @@ let ioDoser    = new gpio(26, 'out');        // GPIO 05 = Saída dosador.
 _log(moduleName, 'Start.');
 
 //Apaga os leds
-//ioShutdown.writeSync(0);
-//ioSensor.writeSync(0);
-//ioLight.writeSync(0);
-//ioDoser.writeSync(0);
+ioShutdown.writeSync(0);
+ioSensor.writeSync(0);
+ioLight.writeSync(0);
+ioDoser.writeSync(0);
 
 let processingShutdown = false;
 let processingSensor = false;
